22dd9005838e22a21c13a958ea9bc751125d21c1
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2020-06-27 00:00:40 +0300

    fix: fix size of image in Modal Controller

--- RSSchool_T6/RSSchool_T6/View Controllers/ModalViewController/ModalViewController.m
+++ RSSchool_T6/RSSchool_T6/View Controllers/ModalViewController/ModalViewController.m
@@ -32,7 +32,7 @@
     self.view.backgroundColor = [UIColor colorFromRGBNumber:@0xFFFFFF];
     self.fullImageView = [[UIImageView alloc] init];
     [self.view addSubview:self.fullImageView ];
-    self.fullImageView.contentMode = UIViewContentModeScaleToFill;
+    self.fullImageView.contentMode = UIViewContentModeScaleAspectFit;
     
     PHCachingImageManager *imageManager = [[PHCachingImageManager alloc] init];
     [imageManager requestImageForAsset:self.asset targetSize:self.fullImageView.bounds.size contentMode:PHImageContentModeDefault options:nil resultHandler:^(UIImage *result, NSDictionary *info)
@@ -56,22 +56,24 @@
 
 -(void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    float aspectRatio = self.fullImageView.image.size.height/self.fullImageView.image.size.width;
+
     [NSLayoutConstraint activateConstraints:@[
         [self.closeButton.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-10],
         [self.closeButton.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:10],
         [self.closeButton.widthAnchor constraintEqualToConstant:40],
         [self.closeButton.heightAnchor constraintEqualToConstant:40],
         
-        [self.fullImageView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:10],
-        [self.fullImageView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-10],
-        [self.fullImageView.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor constant:10],
+        [self.fullImageView.leadingAnchor constraintGreaterThanOrEqualToAnchor:self.view.leadingAnchor constant:10],
+        [self.fullImageView.trailingAnchor constraintLessThanOrEqualToAnchor:self.view.trailingAnchor constant:-10],
+        [self.fullImageView.centerYAnchor constraintLessThanOrEqualToAnchor:self.view.centerYAnchor constant:10],
         [self.fullImageView.topAnchor constraintGreaterThanOrEqualToAnchor:self.view.topAnchor constant:50],
         [self.fullImageView.bottomAnchor constraintLessThanOrEqualToAnchor:self.view.bottomAnchor constant:-50],
+        [self.fullImageView.heightAnchor constraintEqualToAnchor:self.fullImageView.widthAnchor multiplier:aspectRatio],
     ]];
 }
 
 -(void)closeButtonTapped {
     [self dismissViewControllerAnimated:YES completion:nil];
-
 }
 @end
